1625ed1bf76ec8e309c8accf8824da351746480f
YangCang <yangcang_zhang@163.com>
2024-07-29 00:58:59 +0800

    push GitHub first test.

--- src/main/java/cn/zcs/test/GitTest.java
+++ src/main/java/cn/zcs/test/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello world,Once.");
         System.out.println("hello world,master Once.");
         System.out.println("hello world,Twice.");
+        System.out.println("hello GitHub,first push");
     }
 
 }
